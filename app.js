94988ac25ad0d134889ade9096368a47eaed602a
Deepanshi Singhal <deepanshisinghal1523@gmail.com>
2024-09-09 00:33:58 +0530

    completed the basic crud operation using mongodb

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const app = express();
 
+const userModel = require('./userSchema');
+console.log(userModel);
+
 app.get("/", (req,res)=>{
     res.send("hey");
+})
+
+app.get("/create", async (req,res)=>{
+    let createuser = await userModel.create({
+        name:"harsh",
+        email:"harsh@gmail.com",
+        username:"harsh"
+    })
+
+    res.send(createuser);
+})
+
+app.get("/update", async (req,res)=>{
+    let updatedUser = await userModel.findOneAndUpdate({username:"deepanshi"}, {name:"deepanshi singhal"}, {new:true});
+
+    res.send(updatedUser);
+})
+
+app.get("/read", async (req,res)=>{
+    let users = await userModel.find();
+
+    res.send(users);
+})
+
+app.get("/delete", async (req,res)=>{
+    let users = await userModel.findOneAndDelete({username:"harsh"});
+
+    res.send(users);
 })
 
 app.listen(3000);
